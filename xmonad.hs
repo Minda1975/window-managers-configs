4714b443103e531d4de15b0554f35b59d1abc9a1
Mindaugas celiesius <m.celiesius@yandex.ru>
2021-01-24 16:05:43 +0200

    Added some scripts, updated xmonad config

--- xmonad.hs
+++ xmonad.hs
@@ -1,163 +1,35 @@
-------------------------------------------------------------------------
--- import
-------------------------------------------------------------------------
-
-import XMonad hiding ( (|||) ) -- jump to layout
-import XMonad.Layout.LayoutCombinators (JumpToLayout(..), (|||)) -- jump to layout
+import XMonad
 import XMonad.Config.Desktop
-import System.Exit
-import qualified XMonad.StackSet as W
+import XMonad.Util.CustomKeys
+import XMonad.Actions.CycleRecentWS
+import XMonad.Actions.GridSelect
+import XMonad.Actions.DwmPromote
+import XMonad.Actions.NoBorders
 
--- data
-import Data.Char (isSpace)
-import Data.List
-import Data.Monoid
-import Data.Maybe (isJust)
-import Data.Ratio ((%)) -- for video
-import qualified Data.Map as M
-
--- util
-import XMonad.Util.Run (safeSpawn, unsafeSpawn, runInTerm, spawnPipe)
-import XMonad.Util.SpawnOnce
-import XMonad.Util.EZConfig (additionalKeysP, additionalMouseBindings)  
-import XMonad.Util.NamedScratchpad
-import XMonad.Util.NamedWindows
-import XMonad.Util.WorkspaceCompare
-
--- hooks
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks (avoidStruts, docksStartupHook, manageDocks, ToggleStruts(..))
-import XMonad.Hooks.EwmhDesktops -- to show workspaces in application switchers
-import XMonad.Hooks.ManageHelpers (isFullscreen, isDialog,  doFullFloat, doCenterFloat, doRectFloat) 
-import XMonad.Hooks.Place (placeHook, withGaps, smart)
-import XMonad.Hooks.UrgencyHook
-
--- actions
-import XMonad.Actions.CopyWindow -- for dwm window style tagging
-import XMonad.Actions.UpdatePointer -- update mouse postion
-
--- layout 
-import XMonad.Layout.Renamed (renamed, Rename(Replace))
-import XMonad.Layout.Spacing
-import XMonad.Layout.GridVariants
-import XMonad.Layout.ResizableTile
-import XMonad.Layout.BinarySpacePartition
-import XMonad.Layout.NoBorders
-
--- prompts
-import XMonad.Prompt
-import XMonad.Prompt.Window
-
-------------------------------------------------------------------------
--- variables
-------------------------------------------------------------------------
-
-myModMask = mod4Mask -- Sets modkey to super/windows key
-myTerminal = "xterm" -- Sets default terminal
-myBorderWidth = 2 -- Sets border width for windows
-myNormalBorderColor = "#839496"
-myFocusedBorderColor = "#268BD2"
-myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
-------------------------------------------------------------------------
--- layout
-------------------------------------------------------------------------
-
-myLayout = avoidStruts (full ||| tiled ||| grid ||| bsp)
-  where
-     -- full
-     full = renamed [Replace "Full"] 
-          $ noBorders (Full)
-
-     -- tiled
-     tiled = renamed [Replace "Tall"] $ smartBorders
-           $ spacingRaw True (Border 10 0 10 0) True (Border 0 10 0 10) True 
-           $ ResizableTall 1 (3/100) (1/2) []
-
-     -- grid
-     grid = renamed [Replace "Grid"] $ smartBorders
-          $ spacingRaw True (Border 10 0 10 0) True (Border 0 10 0 10) True 
-          $ Grid (16/10)
-
-     -- bsp
-     bsp = renamed [Replace "BSP"] $ smartBorders
-         $ emptyBSP
-
-     -- The default number of windows in the master pane
-     nmaster = 1
-     
-     -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
-
-     -- Percent of screen to increment by when resizing panes
-     delta   = 3/100
-------------------------------------------------------------------------
--- Window rules:
-------------------------------------------------------------------------
-
-myManageHook = composeAll
-    [ className =? "Gimp"           --> doFloat
-    , className =? "mpv"              --> doCenterFloat
-    , className =? "Firefox" <&&> resource =? "Toolkit" --> doFloat -- firefox pip
-    , resource  =? "desktop_window" --> doIgnore
-    , resource  =? "kdesktop"       --> doIgnore 
-    , isFullscreen --> doFullFloat
-    ] <+> namedScratchpadManageHook myScratchpads
-    
-------------------------------------------------------------------------
--- Key bindings. Add, modify or remove key bindings here.
-------------------------------------------------------------------------
-
-myKeys =
-    [("M-" ++ m ++ k, windows $ f i)
-        | (i, k) <- zip (myWorkspaces) (map show [1 :: Int ..])
-        , (f, m) <- [(W.view, ""), (W.shift, "S-"), (copy, "S-C-")]]
-    ++
-    [("S-C-a", windows copyToAll)   -- copy window to all workspaces
-     , ("S-C-z", killAllOtherCopies)  -- kill copies of window on other workspaces
-     , ("M-a", sendMessage MirrorExpand)
-     , ("M-z", sendMessage MirrorShrink)
-     , ("M-s", spawn "dmenu_run") --dmenu
-     , ("M-f", sendMessage $ JumpToLayout "Full")
-     , ("M-t", sendMessage $ JumpToLayout "Tall")
-     , ("M-g", sendMessage $ JumpToLayout "Grid")
-     , ("M-b", sendMessage $ JumpToLayout "BSP")
-     , ("M-p", spawn "gmrun") -- gmrun
-     , ("M-S-g", windowPrompt def Goto wsWindows)
-     , ("M-S-b", windowPrompt def Bring allWindows)
-     , ("S-M-t", withFocused $ windows . W.sink) -- flatten floating window to tiled
-     , ("M-C-<Space>", namedScratchpadAction myScratchpads "terminal")
-     , ("M-C-<Return>", namedScratchpadAction myScratchpads "emacs-scratch")
-    ]
-    
-------------------------------------------------------------------------
--- scratchpads
-------------------------------------------------------------------------
-
-myScratchpads = [ NS "terminal" spawnTerm findTerm manageTerm
-              , NS "emacs-scratch" spawnEmacsScratch findEmacsScratch manageEmacsScratch
-                ] 
+main = xmonad desktopConfig { keys = customKeys delkeys inskeys }
     where
-    role = stringProperty "WM_WINDOW_ROLE"
-    spawnTerm = myTerminal ++  " -name scratchpad"
-    findTerm = resource =? "scratchpad"
-    manageTerm = nonFloating
-    findEmacsScratch = title =? "emacs-scratch"
-    spawnEmacsScratch = "emacsclient -a='' -nc --frame-parameters='(quote (name . \"emacs-scratch\"))'"
-    manageEmacsScratch = nonFloating
+      delkeys :: XConfig l -> [(KeyMask, KeySym)]
+      delkeys XConfig {modMask = modm} =
+          [ (modm .|. shiftMask, xK_Return) -- > terminal
+          , (modm .|. shiftMask, xK_c)      -- > close the focused window
+          ]
+          ++
+          [ (modm .|. m, k) | m <- [0, shiftMask], k <- [xK_w, xK_e, xK_r] ]
 
-------------------------------------------------------------------------
--- main
-------------------------------------------------------------------------
+      inskeys :: XConfig l -> [((KeyMask, KeySym), X ())]
+      inskeys conf@(XConfig {modMask = modm}) =
+          [ ((mod1Mask,             xK_F2  ), spawn $ terminal conf) -- mod1-f2 %! Run a terminal emulator
+          , ((modm,                 xK_Delete), kill) -- %! Close the focused window
+          , ((mod1Mask,             xK_F1 ), spawn "/home/mindaugas/.scripts/osd-time")
+          , ((mod1Mask,             xK_F3 ), spawn "/home/mindaugas/.scripts/osd-sensors")
+          , ((mod1Mask,             xK_F11 ), spawn "i3lock") -- %! Lock the screen
+          , ((modm,                 xK_Return), dwmpromote)
+          , ((modm, xK_Tab),        cycleRecentWS [xK_Alt_L] xK_Tab xK_grave)
+          , ((modm,                 xK_g ),   withFocused toggleBorder)
+          , ((modm,                 xK_w), goToSelected def)
+          , ((modm,                 xK_s), spawnSelected def ["xterm","firefox-esr","pcmanfm","emacs","transmission-gtk"])
+          , ((mod1Mask,             xK_Down), spawn "rofi -show run -sidebar-mode -lines 1") -- %! launcher
+          , ((mod1Mask,             xK_Up  ), spawn "firefox-esr")
+          ]
 
-main = do
-      xmonad $ ewmh desktopConfig
-        { manageHook = ( isFullscreen --> doFullFloat ) <+> myManageHook <+> manageHook desktopConfig
-        , layoutHook         = myLayout
-        , handleEventHook    = handleEventHook desktopConfig
-        , borderWidth        = myBorderWidth
-        , terminal           = myTerminal
-        , modMask            = myModMask
-        , normalBorderColor  = myNormalBorderColor
-        , focusedBorderColor = myFocusedBorderColor
-         }
-          `additionalKeysP` myKeys
+
