9a3a8f941871569d19f741b164ee2b1004a5edf6
Mindaugas Celiesius <m.celiesius@yandex.ru>
2021-02-26 20:48:17 +0200

    Updated configs

--- xmonad.hs
+++ xmonad.hs
@@ -1,35 +1,28 @@
-import XMonad
-import XMonad.Config.Desktop
-import XMonad.Util.CustomKeys
-import XMonad.Actions.CycleRecentWS
-import XMonad.Actions.GridSelect
-import XMonad.Actions.DwmPromote
-import XMonad.Actions.NoBorders
-
-main = xmonad desktopConfig { keys = customKeys delkeys inskeys }
-    where
-      delkeys :: XConfig l -> [(KeyMask, KeySym)]
-      delkeys XConfig {modMask = modm} =
-          [ (modm .|. shiftMask, xK_Return) -- > terminal
-          , (modm .|. shiftMask, xK_c)      -- > close the focused window
-          ]
-          ++
-          [ (modm .|. m, k) | m <- [0, shiftMask], k <- [xK_w, xK_e, xK_r] ]
-
-      inskeys :: XConfig l -> [((KeyMask, KeySym), X ())]
-      inskeys conf@(XConfig {modMask = modm}) =
-          [ ((mod1Mask,             xK_F2  ), spawn $ terminal conf) -- mod1-f2 %! Run a terminal emulator
-          , ((modm,                 xK_Delete), kill) -- %! Close the focused window
-          , ((mod1Mask,             xK_F1 ), spawn "/home/mindaugas/.scripts/osd-time")
-          , ((mod1Mask,             xK_F3 ), spawn "/home/mindaugas/.scripts/osd-sensors")
-          , ((mod1Mask,             xK_F11 ), spawn "i3lock") -- %! Lock the screen
-          , ((modm,                 xK_Return), dwmpromote)
-          , ((modm, xK_Tab),        cycleRecentWS [xK_Alt_L] xK_Tab xK_grave)
-          , ((modm,                 xK_g ),   withFocused toggleBorder)
-          , ((modm,                 xK_w), goToSelected def)
-          , ((modm,                 xK_s), spawnSelected def ["xterm","firefox-esr","pcmanfm","emacs","transmission-gtk"])
-          , ((mod1Mask,             xK_Down), spawn "rofi -show run -sidebar-mode -lines 1") -- %! launcher
-          , ((mod1Mask,             xK_Up  ), spawn "firefox-esr")
-          ]
+import XMonad hiding ( (|||) )  -- don't use the normal ||| operator
+import XMonad.Layout.LayoutCombinators   -- use the one from LayoutCombinators instead
+import XMonad.Util.EZConfig  -- add keybindings easily
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Groups.Examples
+import XMonad.Layout.SimplestFloat
 
 
+main = xmonad myConfig
+
+myConfig = def {
+  --                     
+  layoutHook = smartBorders $ rowOfColumns ||| simplestFloat
+  , terminal = "urxvtc"
+  , modMask  = mod4Mask
+  , normalBorderColor  = "#cccccc"
+  , focusedBorderColor = "#ff69b4"
+  , borderWidth = 2
+  } `additionalKeysP`
+  [ ("M-<F1>",         sendMessage $ JumpToLayout "SimplestFloat")
+  , ("M-M1-<Left>",    zoomWindowOut)
+  , ("M-M1-C-<Right>", zoomWindowIn)
+  , ("M-M1-C-<Up>",    zoomWindowReset)
+  , ("M-M1-C-<Down>",  toggleWindowFull)
+  ]
+    
+                                                
+      
