a166fde12021ccccf671bcbff471c6f9df5eb2ad
Mindaugas Celiesius <m.celiesius@yandex.ru>
2023-12-06 07:50:13 +0200

    Ipdated configs

--- install.sh
+++ install.sh
@@ -22,22 +22,22 @@ sudo apt install -y x11-xserver-utils x11-xkb-utils x11-utils xinit
 sudo apt install -y alsa-utils
 
 #Window manager
-sudo apt install -y notion
+sudo apt install -y ratpoison
 
 #Other stuff
-sudo apt install -y xterm termit dunst libnotify-bin 
+sudo apt install -y xterm  rxvt-unicode dunst libnotify-bin 
 
 # File Manager (eg. pcmanfm,krusader)
-sudo apt install -y emacs w3m mc clex gvfs gvfs-backends gvfs-fuse thunar thunar-volman
+sudo apt install -y emacs w3m mc gvfs gvfs-backends gvfs-fuse thunar thunar-volman
 
 # Printing
 sudo apt install -y cups system-config-printer
 
 # Neofetch/HTOP
-sudo apt install -y neofetch imagemagick caca-utils cmatrix fbcat
+sudo apt install -y neofetch imagemagick cmatrix 
 
 # Various utilities
-sudo apt install -y scrot feh gnome-icon-theme gnome-themes-extra arc-theme gtk2-engines-oxygen gtk2-engines gtk2-engines-pixbuf gtk2-engines-murrine lxappearance
+sudo apt install -y scrot feh gnome-icon-theme gnome-themes-extra arc-theme oxygen-icon-theme gtk2-engines gtk2-engines-pixbuf gtk2-engines-murrine oxygencursors lxappearance
 
 # Browser and Mail client Installation (eg. chromium)
 sudo apt install -y firefox-esr qutebrowser
@@ -46,7 +46,7 @@ sudo apt install -y firefox-esr qutebrowser
 sudo apt install -y xpdf ufw firejail
 
 # Install fonts
-sudo apt install -y fonts-noto xfonts-terminus fonts-roboto
+sudo apt install -y fonts-noto xfonts-terminus fonts-roboto fonts-oxygen
 
 #Music and video packages
 sudo apt install -y sox libsox-fmt-all pipewire-audio cmus moc pavucontrol xfburn
@@ -54,11 +54,14 @@ sudo apt install -y sox libsox-fmt-all pipewire-audio cmus moc pavucontrol xfbur
 #Torrents
 sudo apt install -y qbittorrent
 
-#Install drawnings packages
-sudo apt install -y gimp
+
+#Nvidia
+sudo apt install -y linux-headers-amd64
+
+sudo apt install -y nvidia-tesla-470-driver
 
 #Dev
-sudo apt install -y autoconf build-essential curl texinfo python3-venv rofi
+sudo apt install -y curl python3-venv rofi flatpak
 
 sudo apt autoremove
 
